9f5042133e9b9f8680069e8111516c848e8cd24f
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-01 08:52:20 -0600

    still kinda on the first commit

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const lawnApp = {
 		teeth: true,
 		scissors: false,
 		pushLawnmower: false,
-		startingStudents: false,
+		starvingStudents: false,
 	},
 	start: function () {
 		// alerts to user about the game (when page opens / reloads)
@@ -28,6 +28,59 @@ const lawnApp = {
 		alert("You're starting a landscaping business. Work hard, earn money, buy better cutting tools!")
 		const letsPlay = prompt("Let's play!", "Y")
 		// just 1 choice to play, can hit cancel (already in there)
+		if(letsPlay.toUpperCase() === "Y") {
+			// call teethCut function -- go make one now
+			this.teethCut();
+		}
+	},
+	teethCut: function () {
+		alert(`Go cut some lawns! You get $1 for every cut. Right now, you have $${this.money}.`)
+		// need loop to iterate over money and tools
+		for(let i = 0; i < 5; i++) { // $5 becuase thats how long it will take to get a new tool (make scissors true)
+			const userPlay = prompt("You want to cut some grass with your teeth?", "(Y/N)") //
+			// Y = add $1 to money, N = call showStatus (how much money user has and what their current tool is)
+			// need to go make showStatus now
+			// need to go make userPlay now
+			// DO I NEED ANOTHER forLOOP for when money is over $5 in this part?? then call the scissorsCut function?
+			// make scissors cut function now
+			if(userPlay.toUpperCase() === "Y") {
+				alert("Great! You just made $1! Now get to work!")
+				this.money += 1
+				this.userPlay();
+			} else if (userPlay.toUpperCase() === "N") {
+				this.showStatus();
+			}
+		}
+	},
+	userPlay: function () {
+		if(this.money === 1000 && this.lawnTool === starvingStudents) { // both being the end of the game
+			alert("You won! You're good at business!")
+			return // need to stop the code from running if game is over
+		}
+	},
+
+	showStatus: function () {
+		// need loop to iterate trough the tools array to see what's true.  if this.money is >= 5, then scissors becomes true
+		// and teeth becomes false... and so on
+		// then loop goes until a true is hit, and that tool is called to the function
+		// while this.lawnTOol is false, contiune the loop until true?
+		if (this.money < 5) {alert(`Your tool is ${this.lawnTools}, and you have $${this.money}.`);
+			} else {alert("go make $5 to earn a better tool!");
+			alert(`you have ${this.lawnTools} to cut lawns with right now`); // says you're tool is {object objct}
+			// but the money is adding up!
+		}
+	},
+	scissorsCut: function () {
+
+	},
+	pushLawnmowerCut: function () {
+
+	},
+	batteryLawnmowerCut: function () {
+
+	},
+	startingStudentsCut: function () {
+		
 	}
 }
 
