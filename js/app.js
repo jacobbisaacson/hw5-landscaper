7726c720f3ec96054f6aec81779ba29c09a22ddf
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-01 11:02:26 -0600

    woah, sorry forgot to push for a while

--- js/app.js
+++ js/app.js
@@ -13,78 +13,180 @@ console.log("hw5");
 //You can do this as much as you want.
 
 
-let cutCost = 1;
+
 const lawnApp = {
 	money: 0,
 	lawnTools: {
 		teeth: true,
-		scissors: false,
-		pushLawnmower: false,
-		starvingStudents: false,
+		scissors: true,
+		pushLawnmower: true,
+		starvingStudents: true
+	},
+
+	userPlay: function () {
+		if(this.money === 1000) { 
+		// both being the end of the game
+			alert("You won! You're good at business!")
+			return // need to stop the code from running if game is over
+			}
+			const letsPlay = prompt("Let's play!", "Y/N")
+ 		// just 1 choice to play, can hit cancel (already in there) // call method based on input
+ 			if(letsPlay.toUpperCase() === "Y") {
+		//call teethCut function -- go make one now
+			this.teethCut()
+		} else if (letsPlay.toUpperCase() === "N") {
+			this.showStatus()
+			this.start()
+		}
+	},
+	teethCut: function () {
+		if(this.money <= 5) {
+			this.money += 1
+			// soething here to make this.lawntools to move down the string (or make themn booleans again)
+		}
+		else {
+		alert(`Go cut some lawns! You get $1 for every cut. Right now, you have $${this.money}.`)
+		}
+		// need loop to iterate over money and tools
+		for(let i = 0; i < 5; i++) { // $5 becuase thats how long it will take to get a new tool (make scissors true)
+			const userPlay = prompt("You want to cut some grass with your teeth?", "(Y/N)") //
+			// Y = add $1 to money, N = call showStatus (how much money user has and what their current 
+			// tool is)
+			// need to go make showStatus now
+			// need to go make userPlay now
+			// DO I NEED ANOTHER forLOOP for when money is over $5 in this part?? 
+			// then call the scissorsCut function?
+			// make scissors cut function now
+			if(userPlay.toUpperCase() === "Y") {
+				alert("Great! You just made $1! Now get to work!")
+				this.money += 1
+			} else if(userPlay.toUpperCase() === "N") {
+				alert("k bye!")
+				this.start ();
+			}
+			if(this.money >= 5) {
+				this.scissorsCut();
+			}
+		}
+	},
+	scissorsCut: function () {
+		if(this.money <= 25) {
+			this.money -= 5
+			// need loop for when get's to 25?
+		alert(`Nice! You made enough to buy some sweet Scissors! You make $5 per cut with those, but it just cost you $5! Right now, you have $${this.money}.`)
+		// need loop to iterate over money and tools	
+		} // it's adding $5 when it's called to scissorscut from teech cut??
+		for(let i = this.money; i < 25; i++) { // $5 becuase thats how long it will take to get a new tool 
+			// make scissors true)
+			const userPlayNewTool1 = prompt("You want to cut some grass with your new Scissors?", "(Y/N)") 
+			// Y = add $5 to money, N = call showStatus (how much money user has and what their current 
+			// tool is)
+			// need to go make showStatus now
+			// need to go make userPlay now?  no right?
+			// DO I NEED ANOTHER forLOOP for when money is over $5 in this part?? 
+			// then call the push lawnmower function?
+			// make push lawnmower cut function now
+			if(userPlayNewTool1.toUpperCase() === "Y") {
+				alert("Great! You just made $5! Now get to work!")
+				this.money += 5
+			} else if(userPlayNewTool1.toUpperCase() === "N") {
+				alert("yikes! go back to cutting with your teeth then!")
+				this.teethCut();
+			}
+			if(this.money >= 25) {
+				this.pushLawnmowerCut();
+			}
+		}
+	},
+	pushLawnmowerCut: function () {
+		if(this.money <= 50) {
+			this.money -= 25
+		alert(`Nice! You made enough to buy an old school Push Lawnmower! You make $50 per cut with that, but it just cost you $25! Right now, you have $${this.money}.`)
+		// need loop to iterate over money and tools	
+		}
+		for(let i = this.money; i < 50; i++) {
+			const userPlayNewTool2 = prompt("You want to cut some grass with your new Push Lawnmower?", "(Y/N)") 
+			if(userPlayNewTool2.toUpperCase() === "Y") {
+				alert("Great! You just made $50! Now get to work!")
+				this.money += 50
+			} else if(userPlayNewTool2.toUpperCase() === "N") {
+				alert("yikes! go back to cutting with those scissors then!")
+				this.scissorsCut();
+			}
+			if(this.money >= 250) {
+				this.batteryLawnmowerCut();
+			}
+		}
+
+	},
+	batteryLawnmowerCut: function () {
+		if(this.money <= 500) {
+			this.money -= 250
+		alert(`Nice! You made enough to buy an old school Battery Lawnmower! You make $100 per cut with that, but it just cost you $250! Right now, you have $${this.money}.`)
+		// need loop to iterate over money and tools	
+		}
+		for(let i = this.money; i < 500; i++) {
+			const userPlayNewTool3 = prompt("You want to cut some grass with your new Battery Lawnmower?", "(Y/N)") 
+			if(userPlayNewTool3.toUpperCase() === "Y") {
+				alert("Great! You just made $100! Now get to work!")
+				this.money += 100
+			} else if(userPlayNewTool3.toUpperCase() === "N") {
+				alert("yikes! go back to cutting with that old school mower then!")
+				this.pushLawnmowerCut();
+			}
+			if(this.money >= 500) {
+				this.starvingStudentsCut();
+			}
+		}
+	},
+	starvingStudentsCut: function () {
+		if(this.money <= 1000) {
+			this.money -= 500
+		alert(`Nice! You made enough to hire a team Starving Students! You make $250 with your new team, but just cost you $500 to hire them! Right now, you have $${this.money}.`)
+		// need loop to iterate over money and tools	
+		}
+		for(let i = this.money; i < 1000; i++) {
+			const userPlayNewTool3 = prompt("You want to cut some grass with your sweet new team?", "(Y/N)") 
+			if(userPlayNewTool3.toUpperCase() === "Y") {
+				alert("Great! You just made $250! Now get to work!")
+				this.money += 250
+			} else if(userPlayNewTool3.toUpperCase() === "N") {
+				alert("yikes! go back to cutting with that batter lawnmower then!")
+				this.batteryLawnmowerCut();
+			}
+			if(this.money >= 1000) {
+				this.userPlay();
+			}
+		}
+	},
+	showStatus: function () {
+		alert(`Your tool is ${this.lawnTools}, and you have $${this.money}.`);
+			// says you're tool is {object objct} -0- how to make that say what the current tool is
+			// but the money is adding up! just not saying it in the promps
 	},
 	start: function () {
 		// alerts to user about the game (when page opens / reloads)
 		alert("Landscaping Game! Wooo!")
 		alert("You're starting a landscaping business. Work hard, earn money, buy better cutting tools!")
-		const letsPlay = prompt("Let's play!", "Y")
-		// just 1 choice to play, can hit cancel (already in there)
-		if(letsPlay.toUpperCase() === "Y") {
-			// call teethCut function -- go make one now
-			this.teethCut();
-		}
-	},
-	teethCut: function () {
-		alert(`Go cut some lawns! You get $1 for every cut. Right now, you have $${this.money}.`)
-		// need loop to iterate over money and tools
-		for(let i = 0; i < 5; i++) { // $5 becuase thats how long it will take to get a new tool (make scissors true)
-			const userPlay = prompt("You want to cut some grass with your teeth?", "(Y/N)") //
-			// Y = add $1 to money, N = call showStatus (how much money user has and what their current tool is)
-			// need to go make showStatus now
-			// need to go make userPlay now
-			// DO I NEED ANOTHER forLOOP for when money is over $5 in this part?? then call the scissorsCut function?
-			// make scissors cut function now
-			if(userPlay.toUpperCase() === "Y") {
-				alert("Great! You just made $1! Now get to work!")
-				this.money += 1
-				this.userPlay();
-			} else if (userPlay.toUpperCase() === "N") {
-				this.showStatus();
-			}
-		}
-	},
-	userPlay: function () {
-		if(this.money === 1000 && this.lawnTool === starvingStudents) { // both being the end of the game
-			alert("You won! You're good at business!")
-			return // need to stop the code from running if game is over
-		}
-	},
-
-	showStatus: function () {
-		// need loop to iterate trough the tools array to see what's true.  if this.money is >= 5, then scissors becomes true
-		// and teeth becomes false... and so on
-		// then loop goes until a true is hit, and that tool is called to the function
-		// while this.lawnTOol is false, contiune the loop until true?
-		if (this.money < 5) {alert(`Your tool is ${this.lawnTools}, and you have $${this.money}.`);
-			} else {alert("go make $5 to earn a better tool!");
-			alert(`you have ${this.lawnTools} to cut lawns with right now`); // says you're tool is {object objct}
-			// but the money is adding up!
-		}
-	},
-	scissorsCut: function () {
-
-	},
-	pushLawnmowerCut: function () {
-
-	},
-	batteryLawnmowerCut: function () {
-
-	},
-	startingStudentsCut: function () {
-		
+		this.showStatus();
+		this.userPlay();
 	}
 }
-
 lawnApp.start()
 
 
+///REMOVED FROM TEH START FUNCTION --
+		// const letsPlay = prompt("Let's play!", "Y")
+ 		// just 1 choice to play, can hit cancel (already in there)
+ 		//if(letsPlay.toUpperCase() === "Y") {
+		//call teethCut function -- go make one now
+		//this.teethCut();
+
+
+/// REMOVED FROM SHOW STATUS FUNCTION -- 
+		// need loop to iterate trough the tools array to see what's true.  
+		// if this.money is >= 5, then scissors becomes true
+		// and teeth becomes false... and so on
+		// then loop goes until a true is hit, and that tool is called to the function
+		// while this.lawnTOol is false, contiune the loop until true?
 
